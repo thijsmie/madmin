1962f7353bc035cab0ad30ac17e1b5fa2f83e110
Thijs Miedema <t.miedema@live.nl>
2016-04-21 12:11:00 +0200

    Aaargh

--- factuurinfo/factuurDetail.py
+++ factuurinfo/factuurDetail.py
@@ -162,7 +162,7 @@ class factuurDetail(Container):
 		self.factuurOverigeTotaalOnafgerondLabel = Label(0,0,"Overigetotaal-onafgerond: " + moneyConvert(int(self.factuurOverigeTotaalOnafgerond)))
 		self.factuurOverigeTotaalOnafgerondLabelIdx = self.addChild(0,0,self.factuurOverigeTotaalOnafgerondLabel)
 		self.factuurTotaalOnafgerondLabel = Label(0,0,"Totaal-onafgerond: " + moneyConvert(int(self.factuurBorrelTotaalOnafgerond+self.factuurKantineTotaalOnafgerond+self.factuurEmballageTotaalOnafgerond+self.factuurOverigeTotaalOnafgerond)))
-		self.factuurTotaalOnafgerondLabelIdx = self.addChild(0,0,self.factuurTotaalLabel)
+		self.factuurTotaalOnafgerondLabelIdx = self.addChild(0,0,self.factuurTotaalOnafgerondLabel)
 		
 		self.resize(width, height)
 	
