c95509df149ab5dff1e44508adda875dcdfa39fd
Thijs Miedema <t.miedema@live.nl>
2016-04-21 21:49:46 +0200

    Added a submenu for misc category

--- factuurinfo/factuurList.py
+++ factuurinfo/factuurList.py
@@ -33,12 +33,18 @@ class factuurListItem(Label):
 class factuurList(Container):
 	def __init__(self, width, height, facturen, manager):
 		super(factuurList, self).__init__(width, height)
+		if 'vereniging_id' in facturen[0]:
+		    self.name = getVerenigingNaam(facturen[0]['vereniging_id']).encode('utf-8')
+		else:
+		    self.name = "Leveranciers en overige"
 		self.manager = manager
-		self.factuurListbox = Listbox(width, height)
+		self.header = Label(width, 1, self.name, curses.A_REVERSE) 
+		self.factuurListbox = Listbox(width, height-2)
 		for factuur in reversed(facturen):
 			self.factuurListbox.append(factuurListItem(width, factuur, manager))
 		
-		self.factuurListboxIndex = self.addChild(0,0, self.factuurListbox)
+		self.factuurListboxIndex = self.addChild(0,2, self.factuurListbox)
+		self.headerIndex = self.addChild(0,0, self.header)
 	
 	def keyEvent(self, key):
 		if key == curses.KEY_BACKSPACE:
@@ -49,4 +55,5 @@ class factuurList(Container):
 	def resize(self, width, height):
 		self.width = width
 		self.height = height
-		self.factuurListbox.resize(width, height)
+		self.header.resize(width, 1)
+		self.factuurListbox.resize(width, height-2)
--- factuurinfo/verenigingList.py
+++ factuurinfo/verenigingList.py
@@ -3,6 +3,7 @@ from gui_lib.label import Label
 from gui_lib.listbox import Listbox
 from client_lib.servercall import remote_call
 from factuurList import factuurList
+from factuurOverigeList import factuurOverigeList
 import curses
 
 class verenigingListItem(Label):
@@ -31,7 +32,7 @@ class leverancierListItem(Label):
 	
 	def keyEvent(self, key):
 		if key == ord('\n'):
-			self.manager.push(factuurList(1,1,
+			self.manager.push(factuurOverigeList(1,1,
 				remote_call('/factuur/leverancier'), self.manager))
 	
 	def onFocus(self):
@@ -45,16 +46,16 @@ class verenigingList(Container):
 	def __init__(self, width, height, vereniging_list, manager):
 		super(verenigingList, self).__init__(width, height)
 		self.manager = manager
-		self.header = Label(width, 1, "Madmin Factuurinfo", curses.COLOR_MAGENTA)
-		self.listbox = Listbox(width, height - 1)
+		self.header = Label(width, 1, "Madmin Factuurinfo", curses.A_REVERSE)
+		self.listbox = Listbox(width, height - 2)
 		for vereniging_info in vereniging_list:
 			self.listbox.append(verenigingListItem(width,vereniging_info,manager))
 		self.listbox.append(leverancierListItem(width,manager))
 		self.headerId = self.addChild(0,0, self.header)
-		self.listboxId = self.addChild(0,1,self.listbox)
+		self.listboxId = self.addChild(0,2,self.listbox)
 	
 	def resize(self, width, height):
 		self.width = width
 		self.height = height
 		self.header.resize(width, 1)
-		self.listbox.resize(width, height-1)
+		self.listbox.resize(width, height-2)
